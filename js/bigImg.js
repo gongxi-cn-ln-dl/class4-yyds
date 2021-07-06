a9f51d147392d43d70c91a7c3b30608410adcf99
gongxi_cn_dl <1536660654@qq.com>
2021-07-06 21:47:27 +0800

    增加半年时间轴内容，完成王圣果、王路扬页面

--- js/bigImg.js
+++ js/bigImg.js
@@ -1,11 +1,11 @@
 'use strict'
-// document.getElementById('bigImg').style.visibility="hidden"
+document.getElementById('bigImg').style.visibility="hidden"
 
 function showBigger(folderName,picName,width,height,mode=0) {
     document.getElementById('bigImg').style.visibility="visible"
 
-    let pageWidth = document.body.clientWidth * 0.7
-    let pageHeight = document.body.clientHeight * 0.7
+    let pageWidth = document.body.clientWidth * 0.5
+    let pageHeight = document.body.clientHeight * 0.5
 
     let containerObj = document.getElementById("showImg")
     let imgObj = document.createElement("img")
@@ -20,26 +20,37 @@ function showBigger(folderName,picName,width,height,mode=0) {
     let imgWidth = width
     let imgHeight = height
 
-    containerObj.appendChild(imgObj)
-    // console.log(imgWidth,imgHeight)
-    if(imgHeight <= pageHeight && imgWidth <= pageWidth){
+    // containerObj.appendChild(imgObj)
+    // // console.log(imgWidth,imgHeight)
+    // console.log("222")
+    // if(imgHeight <= pageHeight && imgWidth <= pageWidth){
+    //     console.log("111")
+    //     containerObj.appendChild(imgObj)
+    //     // console.log("123")
+    // }else if(imgHeight > pageHeight || imgWidth > pageWidth){
+    //     console.log("000")
+        // let small_time = imgHeight / pageHeight
+        // let nowImgWidth = imgWidth / small_time
+        // if(nowImgWidth>pageWidth){
+        //     small_time = imgWidth / pageWidth
+        // }
+    if(imgHeight>imgWidth){
+        imgObj.style.height = "80%";
         containerObj.appendChild(imgObj)
-        // console.log("123")
-    }else if(imgHeight > pageHeight || imgWidth > pageWidth){
-        let small_time = imgHeight / pageHeight
-        let nowImgWidth = imgWidth / small_time
-        if(nowImgWidth>pageWidth){
-            small_time = imgWidth / pageWidth
-        }
+    }else{
+        imgObj.style.width = "80%";
+        containerObj.appendChild(imgObj)
+    }
         // imgObj.width = imgWidth / small_time
         // imgObj.height = imgHeight / small_time
-        containerObj.appendChild(imgObj)
 
         // imgObj.style.width = imgWidth / small_time
         // imgObj.style.height = imgHeight / small_time
-    }
+
+        // containerObj.appendChild(imgObj)
+    // }
     imgObj.addEventListener('click',hideBigger)
-    console.log(pageHeight,pageWidth,imgHeight,imgWidth,imgObj.height,imgObj.width)
+    // console.log(pageHeight,pageWidth,imgHeight,imgWidth,imgObj.height,imgObj.width)
 }
 function hideBigger() {
     document.getElementById('bigImg').style.visibility="hidden"
