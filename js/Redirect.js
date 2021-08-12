f46241826ea96eaf32b2ced281f56e746582bc77
gongxi_cn_dl <1536660654@qq.com>
2021-08-12 18:45:37 +0800

    改了一堆

---
+++ js/Redirect.js
@@ -0,0 +1,22 @@
+function Redirect(ID) {
+    let FilesLink;
+    FilesLink = {
+        "陈梓盈": "https://www.aliyundrive.com/s/xDvFWufVvBC",
+        "张博宁": "https://www.aliyundrive.com/s/79F2Esq571w",
+        "王路扬": "https://www.aliyundrive.com/s/mVTSj18DU6D",
+        "王承忠": "https://www.aliyundrive.com/s/EK1fKaCYFZy",
+        "韩昊阳": "https://www.aliyundrive.com/s/oMK5xt9HJZv",
+        "曹雪慧": "https://www.aliyundrive.com/s/7f7BpM6rjwi",
+        "李心雨": "https://www.aliyundrive.com/s/3oni6yCzVPP",
+        "赵琛东": "https://www.aliyundrive.com/s/Sfn1koMcgu5",
+        "王瑞": "https://www.aliyundrive.com/s/aoJDnpAqUgw",
+        "金涵": "https://www.aliyundrive.com/s/jj4VqbbLye3",
+        "考研": "https://www.aliyundrive.com/s/K8S4NkpyxqG",
+        "尹航": "https://www.aliyundrive.com/s/eQtQGS8eKBp",
+        "王圣果": "https://www.aliyundrive.com/s/XXZYz66KDmW",
+        "赵悦竹": "https://www.aliyundrive.com/s/1ArpBpGEMTQ",
+        "未分类": "https://www.aliyundrive.com/s/RCeQgj42ft5"
+    };
+    window.location.href = FilesLink[ID]
+
+}
